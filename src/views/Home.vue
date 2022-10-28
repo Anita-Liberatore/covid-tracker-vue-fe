21106f5545d4475c3d6392b94d6586ffc6017f3c
Anita Liberatore <anitaliberatore@hotmail.com>
2022-10-28 16:47:07 +0200

    finish covid app tracker with vue 2

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,41 +1,53 @@
-template>
 <template>
   <div class="home">
-    <main v-if="!loading" class="home">Show data</main>
+    <main>
+        <DataTitle :text="title" :dataDate="dataDate"/>
+        <DataBoxes :stats="stats"/>
 
-    <main class="flex flex-col align-center justify-center text-center" v-else>
-        <div class="text-gray-500 text-3xl mt-10 mb-6">
-            Fetching Data
-        </div>
+        <CountrySelect @get-country="getCountryData" :countries="countries"/>
+
     </main>
   </div>
 </template>
 
 <script>
+import DataTitle from '@/components/DataTitle.vue'
+import DataBoxes from '@/components/DataBoxes.vue'
+import CountrySelect from '@/components/CountrySelect.vue'
+
+
+
 export default {
   name: "Home",
-  components: {},
+  components: {
+    DataTitle,
+    DataBoxes,
+    CountrySelect
+  },
   data() {
     return {
-      loading: true,
       title: "Global",
       dataDate: "",
-      status: {},
+      stats: {},
       countries: [],
     };
   },
   methods: {
     async fetchCovidData() {
-      const res = await fetch("https://api.covid19api.com/summary");
-      const data = await res.json();
-      return data;
+      const res = await fetch('https://api.covid19api.com/summary');
+      return await res.json();
+    },
+
+    getCountryData(country) {
+      this.stats = country
+      this.title = country.Country
     },
   },
   async created() {
-    const data = this.fetchCovidData();
-    this.dataDate = data.dataDate;
+    const data = await this.fetchCovidData();
+    this.dataDate = data.Date;
     this.stats = data.Global;
-    this.countries = data.Contries;
+    this.countries = data.Countries;
   },
 };
 </script>
